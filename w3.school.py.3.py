6657b9ce5764f2fd40bd41decd5cb1b4c428dd96
fazylbeka <a_fazylbek@kbtu.kz>
2025-02-09 02:57:52 +0500

    синхрон лаб4

---
+++ w3.school.py.3.py
@@ -0,0 +1,73 @@
+#Python Functions
+def my_function():
+    print("Hello from a function")
+
+my_function()
+
+def my_function(fname, lname):
+  print(fname)
+
+
+def my_function(x):
+    return x + 5
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+#Python Lambda
+x =lambda a: a
+
+#Python Arrays
+fruits = ['apple', 'banana', 'cherry']
+print(fruits[0])
+
+fruits = ['apple', 'bananan', 'cherry']
+print(len(fruits))
+
+#Python Classes and Objects
+class MyClass: x = 5
+
+class MyClass:
+  x = 5
+p1 = MyClass()
+
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+class Person:
+    def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def __str__(self):
+    return f"{self.name}({self.age})"
+p1 = Person("John", 36)
+print(p1)
+
+#Python Inheritance
+class Student(Person):
+    class Person:
+        def __init__(self, fname):
+            self.firstname = fname
+
+        def printname(self):
+            print(self.firstname)
+
+    class Student(Person):
+        pass
+
+    x = Student("Mike")
+    x.printname()
+
+list=[1,2,3,4,5,6,7,8,9,10]
+x= lambda a: list
