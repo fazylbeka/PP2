e434e23fc8ea6feaf680d568d5ea5a0dae61be51
fazylbeka <a_fazylbek@kbtu.kz>
2025-02-24 15:16:07 +0500

    Add files via upload

---
+++ json.py
@@ -0,0 +1,52 @@
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+#result: 30
+
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+#result: {"name": "John", "age": 30, "city": "New York"}
+
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+#result: {"name": "John", "age": 30}
+#["apple", "bananas"]
+#["apple", "bananas"]
+#"hello"
+#42
+#31.76
+#true
+#false
+#null
+
