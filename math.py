5e1adbe0855bfc447fec51b586e9677c9ad00bd1
fazylbeka <a_fazylbek@kbtu.kz>
2025-02-24 15:15:51 +0500

    Add files via upload

---
+++ math.py
@@ -0,0 +1,20 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#
+
+x = abs(-7.25)
+
+print(x)
+
+#
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
