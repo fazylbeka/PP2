44bdca04d6d058eaf60b84907df7cd8478a66c41
fazylbeka <a_fazylbek@kbtu.kz>
2025-02-24 15:27:55 +0500

    Add files via upload

--- RegEx.py
+++ RegEx.py
@@ -16,3 +16,29 @@ x = re.search('\s', txt)
 print(x.start())
 #result: 3
 
+#lab5
+
+#1
+import re
+
+txt = "The rain in Spain"
+x = re.search("rain", txt)  # Searches for 'rain' in the string
+
+if x:
+    print("Match found!")
+else:
+    print("No match")
+#result: Match found!
+
+#2
+import re
+
+pattern = re.compile(r"\d+")  # Compile regex to match digits
+txt = "There are 123 apples and 456 bananas"
+
+matches = pattern.findall(txt)  # Find all matches
+print(matches)
+#result: ['123', '456']
+
+
+
